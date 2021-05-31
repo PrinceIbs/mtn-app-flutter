535fc8539dc83cdc57945a1e14b13255caf1169a
Suleiman Ibrahim <suleimanonnet@gmail.com>
2021-05-31 20:33:06 +0100

    Before adding the Navigation functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,414 @@
-import 'package:flutter/material.dart';
+import 'dart:ffi';
+
+import "package:flutter/material.dart";
 
 void main() {
-  runApp(MyApp());
+  runApp(HomeClass());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class HomeClass extends StatelessWidget {
+  final List<String> entries = <String>['A', 'B', 'C'];
+  final List<int> colorCodes = <int>[600, 500, 100];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      title: 'mtn',
+      home: SafeArea(
+        child: Scaffold(
+          drawer: Drawer(
+            child: Container(
+              color: Colors.black.withOpacity(.60),
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 20.0, bottom: 20, right: 20, left: 15),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.black,
+                          child: Icon(
+                            Icons.person,
+                            size: 25,
+                            color: Colors.yellow,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 20.0, bottom: 20, right: 20, left: 15),
+                        child: Column(
+                          children: [
+                            Text(
+                              "Welcome",
+                              style: TextStyle(
+                                fontSize: 19,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white,
+                                letterSpacing: 2.3,
+                              ),
+                            ),
+                            Text(
+                              "USERNAME",
+                              style: TextStyle(
+                                fontSize: 26,
+                                fontWeight: FontWeight.w600,
+                                fontFamily: 'monospace',
+                                color: Colors.white,
+                                letterSpacing: 2.5,
+                              ),
+                            ),
+                            Text(
+                              "09081927384",
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white,
+                                letterSpacing: 1,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Divider(
+                    height: 1.0,
+                    thickness: 1.0,
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.message),
+                    title:
+                        Text('Messages', style: TextStyle(color: Colors.white)),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.account_circle),
+                    title:
+                        Text('Profile', style: TextStyle(color: Colors.white)),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title:
+                        Text('Settings', style: TextStyle(color: Colors.white)),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.message),
+                    title:
+                        Text('Messages', style: TextStyle(color: Colors.white)),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.account_circle),
+                    title:
+                        Text('Profile', style: TextStyle(color: Colors.white)),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title:
+                        Text('Settings', style: TextStyle(color: Colors.white)),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.message),
+                    title:
+                        Text('Messages', style: TextStyle(color: Colors.white)),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.account_circle),
+                    title:
+                        Text('Profile', style: TextStyle(color: Colors.white)),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text('SettingsNow',
+                        style: TextStyle(color: Colors.white)),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.message),
+                    title:
+                        Text('Messages', style: TextStyle(color: Colors.white)),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.account_circle),
+                    title:
+                        Text('Profile', style: TextStyle(color: Colors.white)),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          appBar: AppBar(
+            backgroundColor: Colors.yellow[700],
+            title: Text(
+              "+23480987654",
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 16,
+                color: Colors.black,
+              ),
             ),
-          ],
+            centerTitle: true,
+            actions: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Center(
+                  child: Icon(
+                    Icons.mail_outline,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                ),
+              ),
+              // Padding(
+              //   padding: const EdgeInsets.all(4.0),
+              //   child: Image.network(
+              //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSACqU_2tEwu-x4J8P4O1uGvTV1GWOE_QkNpIAkMDyGiVRmkutgP4Heit_X3dP1XuNQm5s&usqp=CAU",
+              //     height: 42,
+              //     width: 43,
+              //   ),
+              // ),
+              Container(
+                height: 20,
+                width: 30,
+                padding: EdgeInsets.all(1.2),
+                margin: EdgeInsets.only(
+                  top: 12,
+                  bottom: 12,
+                  right: 10,
+                  left: 10,
+                ),
+                color: Colors.white,
+                child: Image(
+                  image: AssetImage('assets/images/mtn-icon.jpg'),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            selectedItemColor: Colors.yellow,
+            unselectedItemColor: Colors.white.withOpacity(.60),
+            selectedFontSize: 12.0,
+            unselectedFontSize: 12.0,
+            //backgroundColor: Colors.blueGrey,
+            items: [
+              BottomNavigationBarItem(
+                title: Text("Home", style: TextStyle(color: Colors.black)),
+                icon: IconButton(
+                  icon: Icon(Icons.home, color: Colors.black),
+                  onPressed: () {
+                    print("You tapped HOME");
+                  },
+                ),
+                // label: 'Bottom',
+              ),
+              BottomNavigationBarItem(
+                title: Text("Recharge", style: TextStyle(color: Colors.black)),
+                icon: IconButton(
+                  icon: Icon(Icons.play_arrow, color: Colors.black),
+                  onPressed: () {
+                    print("You tapped RECHARGE");
+                  },
+                ),
+              ),
+              BottomNavigationBarItem(
+                title: Text("Bundle", style: TextStyle(color: Colors.black)),
+                icon: IconButton(
+                  icon: Icon(Icons.shopping_basket_sharp, color: Colors.black),
+                  onPressed: () {
+                    print("You tapped BUNDLE");
+                  },
+                ),
+              ),
+              // BottomNavigationBarItem(
+              //   title: Text("Shop", style: TextStyle(color: Colors.black)),
+              //   icon: IconButton(
+              //     icon: Icon(Icons.play_circle_outline_sharp,
+              //         color: Colors.black),
+              //     onPressed: () {
+              //       print("You tapped PLAY");
+              //     },
+              //   ),
+              // ),
+              // BottomNavigationBarItem(
+              //   title: Text("Shop", style: TextStyle(color: Colors.black)),
+              //   icon: IconButton(
+              //     icon: Icon(Icons.apps_outlined, color: Colors.black),
+              //     onPressed: () {
+              //       print("You tapped MORE");
+              //     },
+              //   ),
+              // ),
+            ],
+          ),
+          body: Container(
+            color: Colors.grey.shade50,
+            child: ListView(
+              children: [
+                Container(
+                  color: Colors.grey,
+                  margin: EdgeInsets.all(5),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 20.0, bottom: 20, right: 20, left: 15),
+                        child: CircleAvatar(
+                          backgroundColor: Colors.yellow[800],
+                          child: Icon(
+                            Icons.person,
+                            size: 35,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      Text("Good Morning USERNAME"),
+                    ],
+                  ),
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Container(
+                        color: Colors.greenAccent,
+                        height: 65,
+                        width: 360,
+                      ),
+                      Container(
+                        color: Colors.redAccent,
+                        height: 65,
+                        width: 360,
+                      ),
+                      Container(
+                        color: Colors.black,
+                        height: 65,
+                        width: 360,
+                      ),
+                      Container(
+                        color: Colors.blueAccent,
+                        height: 65,
+                        width: 360,
+                      ),
+                      Container(
+                        color: Colors.blueGrey,
+                        height: 65,
+                        width: 360,
+                      ),
+                      Container(
+                        color: Colors.limeAccent,
+                        height: 65,
+                        width: 360,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(5),
+                  child: Column(
+                    children: [
+                      Text("Get Welcome Back Data"),
+                      Text(
+                        "Click Here",
+                        style: TextStyle(
+                          color: Colors.yellow[800],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  color: Colors.grey,
+                  child: Row(
+                    children: [
+                      Container(
+                        child: Icon(Icons.shopping_bag,
+                            color: Colors.yellow, size: 30),
+                        padding: EdgeInsets.all(10),
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            Text(
+                              "Your NIN Status",
+                              style: TextStyle(color: Colors.blue),
+                            ),
+                            Text(
+                              "NIN submitted - Verification Ongoing",
+                            ),
+                          ],
+                        ),
+                      ),
+                      Icon(Icons.chevron_right, size: 30),
+                    ],
+                  ),
+                ),
+                Text("QUICK ACCESS"),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.all(7),
+                        color: Colors.red,
+                        width: 90,
+                        height: 90,
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(7),
+                        color: Colors.orange,
+                        width: 90,
+                        height: 90,
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(7),
+                        color: Colors.yellow,
+                        width: 90,
+                        height: 90,
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(7),
+                        color: Colors.green,
+                        width: 90,
+                        height: 90,
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(7),
+                        color: Colors.blue,
+                        width: 90,
+                        height: 90,
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(7),
+                        color: Colors.indigo,
+                        width: 90,
+                        height: 90,
+                      ),
+                      Container(
+                        margin: EdgeInsets.all(7),
+                        color: Colors.deepPurple,
+                        width: 90,
+                        height: 90,
+                      ),
+                    ],
+                  ),
+                ),
+                Text("My Plan"),
+              ],
+            ),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class ButtomNavScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
   }
 }
